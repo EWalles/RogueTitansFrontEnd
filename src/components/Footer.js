be5c803d0560d6fe8fb93483762ae525288d3b1a
EWalles <ejomcdonald@gmail.com>
2022-09-30 08:57:32 -0400

    building pages

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class Footer extends PureComponent {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+Footer.propTypes = {
+
+};
+
+export default Footer;
---
+++ src/pages/DndDisclaimer.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class DndDisclaimer extends PureComponent {
+  render() {
+    return (
+      <div>
+        “'The Rogue Tavern' is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.”
+      </div>
+    )
+  }
+}
