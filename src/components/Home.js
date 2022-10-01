4c8c61ea2e6af2b6b3e55b3d02eb9d9b4da1278c
Zachary Gexler <110911839+zachgexler@users.noreply.github.com>
2022-10-01 13:05:24 -0400

    api framework on FE

---
+++ src/components/Home.js
@@ -0,0 +1,27 @@
+//Header.js
+import {useState, useEffect } from 'react';
+
+function Home(props) {
+
+    const [state, setState ] = useState([]);
+
+    useEffect( () => {
+        fetch('https://www.dnd5eapi.co/api/')
+        .then(res => res.json())
+        .then( (result) => {
+            setState(result);
+        }), (error) => {
+            console.log("error")
+        }
+    })
+
+    return (
+        <div>
+            <h5>
+                {state.classes}
+            </h5>
+        </div>
+    )
+}
+
+export default Home;
