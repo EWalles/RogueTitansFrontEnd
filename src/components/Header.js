e6310949accd885d49228ce61ef9cc0824b52cb3
EWalles <ejomcdonald@gmail.com>
2022-10-03 10:38:53 -0400

    added logo and header page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 //Header.js
 import { Link } from "react-router-dom"
+import {logo} from "public/gallery/logo.png"
 
 function Header(props) {
     return (
         <nav className="nav">
             <Link to="/">
+                <div>{logo}</div>
                 <div>The Rogue Tavern</div>
             </Link>
             
