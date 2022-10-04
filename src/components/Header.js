dfeba45e0abd91cc4a91460e69204d8c3a4abec5
EWalles <ejomcdonald@gmail.com>
2022-10-04 09:52:25 -0400

    updates to pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,18 @@
 //Header.js
 import React from "react"
 import { Link, Route } from "react-router-dom"
+import Navbar from "./Nav"
 
 function Header() {
     return (
         <nav className="nav">
             <Route>
-                <Link to="/">
+            <Link to="/">
                     <div>The Rogue Tavern</div>
-                </Link>
+            </Link>
+                <div>
+                    <Navbar />
+                </div>
             </Route>
         </nav>
     )
---
+++ src/pages/StatSheet.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+import DnDCharacterStatsSheet from 'dnd-character-sheets'
+import 'dnd-character-sheets/dist/index.css'
+
+class Example extends Component {
+  render() {
+    return <DnDCharacterStatsSheet />
+  }
+}
