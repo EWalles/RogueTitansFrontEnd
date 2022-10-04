c4ff9fda31189758a734e24497b249cce70e6f21
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-10-04 13:52:14 -0400

    created navbar folder with js files within components

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,34 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName:'nav-links',
+
+    },
+    {
+        title: 'About',
+        url: '#',
+        cName:'nav-links',
+
+    },  {
+        title: 'Sign In',
+        url: '#',
+        cName:'nav-links',
+
+    },  {
+        title: 'Sign Up',
+        url: '#',
+        cName:'nav-links',
+
+    },{
+        title: 'Character Sheet',
+        url: '#',
+        cName:'nav-links',
+
+    },  {
+        title: 'Drop down',
+        url: '#',
+        cName:'nav-links-mobile',
+
+    },
+]
