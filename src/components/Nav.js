0482aab42d4f0c56aa603abbf49c8facc08fb5fe
EWalles <ejomcdonald@gmail.com>
2022-10-04 09:49:11 -0400

    added character sheets

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import { Link, Router } from "react-router-dom"
+
 export default function Nav() {
   return (
     <div>
-    <Router>
-      <Link> Home</Link>
-    </Router>
+      
     </div>
   )
 }
