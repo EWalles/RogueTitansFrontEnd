d0c82ed92bac9541d0458c0d0931d0ba51ef9c45
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-05 19:18:39 -0400

    starting authentication with firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,9 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCsqyRRz7L-DfKCcNu4ppki6Q6RllyUKzc",
+    authDomain: "rogouetitan.firebaseapp.com",
+    projectId: "rogouetitan",
+    storageBucket: "rogouetitan.appspot.com",
+    messagingSenderId: "148854565736",
+    appId: "1:148854565736:web:2f658574713c429a8384dd"
+  };
