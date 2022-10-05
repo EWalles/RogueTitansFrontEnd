2dc810d65fa2bccdef68c0e42eb10ff2f43fca6d
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-10-05 01:56:56 -0400

    modded icons, created function to indicate menu being open/closed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,24 @@
 //Header.js
-<<<<<<< HEAD
-import { Link } from "react-router-dom"
-import {logo} from "public/gallery/logo.png"
-=======
+// import {logo} from "public/gallery/logo.png"
+import Navbar from "./Navbar/Navbar"
+
 import React from "react"
 import { Link, Route } from "react-router-dom"
-<<<<<<< HEAD
-import Navbar from "./Nav"
-=======
->>>>>>> 13455c3e6e90d17ce8a3315bfdfb89d5de430946
->>>>>>> 6374586637972ca13445f2bc4a7a2584db4f8d0d
+
+
 
 function Header() {
     return (
         <nav className="nav">
-<<<<<<< HEAD
-            <Link to="/">
-                <div>{logo}</div>
-                <div>The Rogue Tavern</div>
-            </Link>
-            
-=======
             <Route>
             <Link to="/">
                     <div>The Rogue Tavern</div>
             </Link>
                 <div>
-                    <Navbar />
+
                 </div>
             </Route>
->>>>>>> 13455c3e6e90d17ce8a3315bfdfb89d5de430946
+
         </nav>
     )
 }
