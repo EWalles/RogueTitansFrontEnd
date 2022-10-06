fd70a016196f518485e73a0111519fc91c90f21d
Samim Kohistani <samimkohistani@samims-air.myfiosgateway.com>
2022-10-06 14:25:30 -0400

    login form displays

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,26 +1,26 @@
-// import React from 'react'
+import React from 'react'
 
-// function Loginform() {
-//   return (
-//     <form>
-//         <div className='form-inner'>
-//             <h2>Login</h2>
-//             {/* {error} */}
-//             <div className='form-group'>
-//                 <label htmlFor='"name'>Name:</label>
-//                 <input type="text" name="name" id="name"/>
-//             </div>
-//             <div className='form-group'>
-//                 <label htmlFor='email'>Email:</label>
-//                 <input type="email" name="email" id="email" />
-//             </div>
-//             <div className='form-group'>
-//                 <label htmlFor='password'>password:</label>
-//                 <input type='password' name='password' id='password'/>
-//             </div>
-//         </div>
-//     </form>
-//   )
-// }
+function Loginform() {
+  return (
+    <form>
+        <div className='form-inner'>
+            <h2>Login</h2>
+            {/* {error} */}
+            <div className='form-group'>
+                <label htmlFor='"name'>Name:</label>
+                <input type="text" name="name" id="name"/>
+            </div>
+            <div className='form-group'>
+                <label htmlFor='email'>Email:</label>
+                <input type="email" name="email" id="email" />
+            </div>
+            <div className='form-group'>
+                <label htmlFor='password'>password:</label>
+                <input type='password' name='password' id='password'/>
+            </div>
+        </div>
+    </form>
+  )
+}
 
-// export default Loginform
+export default Loginform
